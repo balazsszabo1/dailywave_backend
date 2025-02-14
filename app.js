e583f65825c1100d47d1611b8482cfc8389b96bd
diak <diak@mesterlevel.lo>
2025-02-14 12:05:45 +0100

    cors

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(limiter);
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://127.0.0.1:5500',
+    origin: 'https://deft-moonbeam-90e218.netlify.app',
     credentials: true
 }));
 
