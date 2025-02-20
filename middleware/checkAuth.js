28a9013f4555c55d9004b47c706746e5c1eba332
diak <diak@mesterlevel.lo>
2025-02-20 12:40:11 +0100

    cookie mukodjon

---
+++ middleware/checkAuth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../config/dotenvConfig').config;
+
+const checkAuth = (req, res, next) => {
+    const token = req.cookies.auth_token || req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ error: 'Nincs érvényes token, kérlek jelentkezz be' });
+    }
+
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).json({ error: 'Érvénytelen token' });
+        }
+        req.user = decoded;
+        next();
+    });
+};
+
+module.exports = checkAuth;
