78533ef33e193cc85f1ab60434d7800dab45e4ed
Joncs <scilling.janos.attila.1046@dszcbaross.edu.hu>
2025-03-12 10:52:12 +0100

    hir lekerdezes

--- routes/newsRoutes.js
+++ routes/newsRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const authenticateToken = require('../middleware/jwtAuth');
-const { uploadNews } = require('../controllers/newsControllers')
+const { uploadNews, getAllNews } = require('../controllers/newsControllers')
 
 const router = express.Router();
 
 router.post('/uploadnews', authenticateToken, uploadNews);
+router.get('/getAllNews', authenticateToken, getAllNews);
 
 module.exports = router;
