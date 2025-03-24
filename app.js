9811be301d972f0bd1453cdab2be9eabcddc623b
balazsszabo <szabo.balazs.1048@dszcbaross.edu.hu>
2025-03-24 12:16:11 +0100

    hírek képmegjelenités

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(cors({
 }));
 
 // Statikus fájlok elérhetősége
-app.use('/uploads', authenticateToken, express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/api/auth', authRoutes)
 app.use('/api/topics', topicRoutes)
