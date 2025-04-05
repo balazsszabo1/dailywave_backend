3c740ae214ef9d3710ce6e8dbb4764549a49cac8
Joncsi <schilling.janos.attila.1046@dszcbaross.edu.hu>
2025-04-05 15:46:57 +0200

    topic upload

--- routes/topicRoutes.js
+++ routes/topicRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const authenticateToken = require('../middleware/jwtAuth');
-const upload = require('../middleware/multer');
+// const upload = require('../middleware/multer'); // nem kell most
 const { getAlltopics, getComments, addComment, uploadTopic } = require('../controllers/topicControllers');
 
 const router = express.Router();
 
 router.get('/getAlltopics', authenticateToken, getAlltopics);
-router.post('/uploadTopic', upload.single('topic'), uploadTopic);
+router.post('/uploadTopic', authenticateToken, uploadTopic); 
 router.get('/getComments/:topicId', getComments);
-router.post('/addComment', addComment);
+router.post('/addComment', authenticateToken, addComment);
 
 module.exports = router;
